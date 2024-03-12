aa047d2fd07af8b4bc9797e733355408572543be
Atli Axfjörð <atliax@gmail.com>
2024-03-12 01:46:06 +0000

    Færði classana neðst og game loop dótið efst

--- main.js
+++ main.js
@@ -1,3 +1,52 @@
+const canvas = document.getElementById('mainCanvas');
+const context = canvas.getContext('2d');
+
+const KEY_LEFT = 37;
+const KEY_RIGHT = 39;
+const KEY_UP = 38;
+const KEY_DOWN = 40;
+const KEY_SPACE = 32;
+
+context.strokeStyle = "white";
+context.fillStyle = "black";
+context.lineWidth = 1;
+
+let grid = 16;
+
+let playerStartX = context.canvas.width/2;
+let playerStartY = context.canvas.height/2;
+let playerSpeed = 0.5;
+let playerRotationSpeed = 15;
+
+let asteroidStartX = Math.floor(Math.random()*(context.canvas.width-1));
+let asteroidStartY = Math.floor(Math.random()*(context.canvas.height-1));
+
+document.addEventListener("keydown",keydown);
+document.addEventListener("keyup",keyup);
+
+let game = setInterval(update, 20);
+
+function update()
+{
+    player.move();
+    asteroid.move();
+    asteroid.rotate();
+    asteroid2.move();
+    asteroid2.rotate();
+
+    //skoða árekstra hér?
+
+    //hreinsa skjáinn með því að teikna kassa
+    context.clearRect(0,0,context.canvas.width,context.canvas.height);
+    context.beginPath();
+    context.rect(0,0,context.canvas.width,context.canvas.height);
+    context.fill();
+
+    player.draw();
+    asteroid.draw();
+    asteroid2.draw();
+}
+
 function keydown(event)
 {
     if(event.keyCode == KEY_LEFT)//vinstri
@@ -28,6 +77,59 @@ function keyup(event)
         player.thrust(0);
         player.m_moving = false
     } 
+}
+
+function randomShape(X,Y,nodes,minR,maxR)
+{
+    let angleStep = (Math.PI * 2) / nodes;
+
+    let tmpRet = [];
+    let x, y;
+
+    for(let i = 0;i < nodes;i++)
+    {
+        do
+        {
+            let targetAngle = angleStep * i;
+            let angle = targetAngle + (Math.random() - 0.75) * angleStep * 0.5;
+            let radius = minR + Math.random() * (maxR-minR);
+            x = Math.round(Math.round((Math.cos(angle) * radius) * grid)/grid);
+            y = Math.round(Math.round((Math.sin(angle) * radius) * grid)/grid);
+        }
+        while(isXYInArray(x,y,tmpRet));
+
+        tmpRet.push([x,y]);
+    }
+
+    return tmpRet;
+}
+
+function isXYInArray(tx,ty,tmpRet)
+{
+    if(tmpRet.length == 0)
+        return false;
+
+    for(let j = 0; j < tmpRet.length;j++)
+    {
+        if(tmpRet[j][0] == tx && tmpRet[j][1] == ty)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function degToRad(a)
+{
+    return a * (Math.PI/180);
+}
+
+function rotate([X,Y],angle)
+{
+    let nX = X*Math.cos(degToRad(angle))-Y*Math.sin(degToRad(angle));
+    let nY = X*Math.sin(degToRad(angle))+Y*Math.cos(degToRad(angle));
+    return [nX,nY];
 }
 
 class Polygon
@@ -223,108 +325,6 @@ class Asteroid extends Polygon
     }
 }
 
-function randomShape(X,Y,nodes,minR,maxR)
-{
-    let angleStep = (Math.PI * 2) / nodes;
-
-    let tmpRet = [];
-    let x, y;
-
-    for(let i = 0;i < nodes;i++)
-    {
-        do
-        {
-            let targetAngle = angleStep * i;
-            let angle = targetAngle + (Math.random() - 0.75) * angleStep * 0.5;
-            let radius = minR + Math.random() * (maxR-minR);
-            x = Math.round(Math.round((Math.cos(angle) * radius) * grid)/grid);
-            y = Math.round(Math.round((Math.sin(angle) * radius) * grid)/grid);
-        }
-        while(isXYInArray(x,y,tmpRet));
-
-        tmpRet.push([x,y]);
-    }
-
-    return tmpRet;
-}
-
-function isXYInArray(tx,ty,tmpRet)
-{
-    if(tmpRet.length == 0)
-        return false;
-
-    for(let j = 0; j < tmpRet.length;j++)
-    {
-        if(tmpRet[j][0] == tx && tmpRet[j][1] == ty)
-        {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-function degToRad(a)
-{
-    return a * (Math.PI/180);
-}
-
-function rotate([X,Y],angle)
-{
-    let nX = X*Math.cos(degToRad(angle))-Y*Math.sin(degToRad(angle));
-    let nY = X*Math.sin(degToRad(angle))+Y*Math.cos(degToRad(angle));
-    return [nX,nY];
-}
-
-function update()
-{
-    player.move();
-    asteroid.move();
-    asteroid.rotate();
-    asteroid2.move();
-    asteroid2.rotate();
-
-    //skoða árekstra hér?
-
-    //hreinsa skjáinn með því að teikna kassa
-    context.clearRect(0,0,context.canvas.width,context.canvas.height);
-    context.beginPath();
-    context.rect(0,0,context.canvas.width,context.canvas.height);
-    context.fill();
-
-    player.draw();
-    asteroid.draw();
-    asteroid2.draw();
-}
-
-const canvas = document.getElementById('mainCanvas');
-const context = canvas.getContext('2d');
-
-const KEY_LEFT = 37;
-const KEY_RIGHT = 39;
-const KEY_UP = 38;
-const KEY_DOWN = 40;
-const KEY_SPACE = 32;
-
-context.strokeStyle = "white";
-context.fillStyle = "black";
-context.lineWidth = 1;
-
-let grid = 16;
-
-let playerStartX = context.canvas.width/2;
-let playerStartY = context.canvas.height/2;
-let playerSpeed = 0.5;
-let playerRotationSpeed = 15;
-
-let asteroidStartX = Math.floor(Math.random()*(context.canvas.width-1));
-let asteroidStartY = Math.floor(Math.random()*(context.canvas.height-1));
-
-document.addEventListener("keydown",keydown);
-document.addEventListener("keyup",keyup);
-
 let player = new Ship(playerStartX,playerStartY);
 let asteroid = new Asteroid(asteroidStartX,asteroidStartY);
 let asteroid2 = new Asteroid(asteroidStartX,asteroidStartY);
-
-let game = setInterval(update, 20);
